26c6b4d4a0b3790cb618f25ee554bc5dc9b1d9ef
Milan Curcic <caomaco@gmail.com>
2016-06-29 15:29:04 -0400

    run for different values of a

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for kl in {0.5,1,2,4,6};do
-    for a in {0.001,0.01,0.1};do
+for kl in {1,2,4,6};do
+    for a in {0.05,0.075,0.1};do
         echo kl = ${kl}, a = ${a}
-        python wavenumber.py $kl $a 60
+        python wavenumber.py $kl $a 110 &
     done
 done
